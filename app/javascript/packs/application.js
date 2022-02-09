481fb96ee3176fb4870ca973d34a20438e654ee7
rajithsoza <rajithsoza.esq@gmail.com>
2022-02-09 01:16:19 +0000

    rajupdate

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -20,6 +20,7 @@ ActiveStorage.start()
 
 // External imports
 import "bootstrap";
+import "../stylesheets/application"
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
