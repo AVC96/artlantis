2a531d8923989924cd31d5be498b4152438f80b2
Rahul Keerthi <rahulkeerthi2@gmail.com>
2022-02-17 00:31:55 +0000

    refactor(*): style fixes

--- app/controllers/arts_controller.rb
+++ app/controllers/arts_controller.rb
@@ -1,16 +1,8 @@
 class ArtsController < ApplicationController
-  skip_before_action :authenticate_user! , only: :index
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     @arts = Art.all
-    # @arts = Art.where.not(latitude: nil, longitude: nil)
-    # # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
-    # @markers = @arts.geocoded.map do |art|
-    #   {
-    #     lat: art.latitude,
-    #     lng: art.longitude
-    #   }
-    # end
   end
 
   def show
@@ -23,11 +15,15 @@ class ArtsController < ApplicationController
 
   def create
     @art = Art.new(art_params)
-    @art.save
-    redirect_to art_path(@art)
+    if @art.save!
+      redirect_to art_path(@art)
+    else
+      render 'new'
+    end
   end
 
   private
+
   def art_params
     params.require(:art).permit(:cost, :location, :type, :description, :image)
   end
