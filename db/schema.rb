ef8c983b299445f41549f9a1c1f51889c2272449
Audrey Handem <audrey@speedinvest.com>
2022-02-22 18:46:31 +0000

    changes from last week, Rahul

--- db/schema.rb
+++ db/schema.rb
@@ -64,6 +64,15 @@ ActiveRecord::Schema.define(version: 2022_02_17_092015) do
     t.index ["artist_id"], name: "index_arts_on_artist_id"
   end
 
+  create_table "favourites", force: :cascade do |t|
+    t.bigint "art_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["art_id"], name: "index_favourites_on_art_id"
+    t.index ["user_id"], name: "index_favourites_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -79,4 +88,6 @@ ActiveRecord::Schema.define(version: 2022_02_17_092015) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "arts", "artists"
+  add_foreign_key "favourites", "arts"
+  add_foreign_key "favourites", "users"
 end
