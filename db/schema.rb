8fd22ed0ade85c9d5d6c636b9b3d558c818c5c26
Rahul Keerthi <rahulkeerthi2@gmail.com>
2022-03-01 20:21:02 +0000

    schema update

--- db/schema.rb
+++ db/schema.rb
@@ -77,15 +77,6 @@ ActiveRecord::Schema.define(version: 2022_02_26_152555) do
     t.index ["user_id"], name: "index_bids_on_user_id"
   end
 
-  create_table "favourites", force: :cascade do |t|
-    t.bigint "art_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["art_id"], name: "index_favourites_on_art_id"
-    t.index ["user_id"], name: "index_favourites_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -103,6 +94,4 @@ ActiveRecord::Schema.define(version: 2022_02_26_152555) do
   add_foreign_key "arts", "artists"
   add_foreign_key "bids", "arts"
   add_foreign_key "bids", "users"
-  add_foreign_key "favourites", "arts"
-  add_foreign_key "favourites", "users"
 end
