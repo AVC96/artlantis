e535b98c4590f76f0499ad7493a7f96063253b96
Molly Gao <mollygaocareer@gmail.com>
2022-03-05 15:30:58 +0000

    has_many :bids changed

--- app/models/art.rb
+++ app/models/art.rb
@@ -29,7 +29,7 @@ class Art < ApplicationRecord
   # IMAGE ATTACHMENTS (MULTIPLE POSSIBLE WITH HAS_MANY_ATTACHED)
   has_many_attached :images
 
-  has_many :bid
+  has_many :bids
   # VALIDATIONS
   # WHICH FIELDS ARE REQUIRED FOR A SINGLE ARTWORK?
 end
