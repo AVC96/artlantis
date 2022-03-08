c73164e0621ada6f39816637ac63cd71cfc06ba0
Audrey Handem <audrey@speedinvest.com>
2022-03-08 20:56:50 +0000

    created an art oage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  skip_before_action :authenticate_user!, only: [:home, :contents]
   def home
     @arts = Art.all.sample(3)
   end
 
   def myauctions
     # Add temp code for showing artworks
+  end
+
+  def contents
   end
 end
