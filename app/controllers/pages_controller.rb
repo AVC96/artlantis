6b981d567e644f55b5e0eaa1532ea3fcd41a8fea
Rahul Keerthi <rahulkeerthi2@gmail.com>
2022-03-10 19:24:55 +0000

    my_auctions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,12 +3,8 @@ class PagesController < ApplicationController
   def home
     @arts = Art.all.sample(3)
   end
+
   def my_auctions
-    # Add temp code for showing artworks
-    @bids = Bid.where(user_id: current_user.id)
-    # @bid.user = current_user
-    @art = Art.where(id:params[:art_id])
-    raise
-    # @bid.art = art
+    @artworks = Art.joins(:bids).where(bids: { user_id: current_user.id }).distinct
   end
 end
